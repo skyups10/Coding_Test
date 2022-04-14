00488c57d4d67a7a6592b714e70e9c1ff504af58
Minkyung Jang <70564788+skyups10@users.noreply.github.com>
2022-04-14 22:32:10 +0900

    Update 그리디.py

--- 강의/그리디.py
+++ 강의/그리디.py
@@ -1,3 +1,7 @@
+# 2강. 그리디
+# 그리디 = 탐욕법 = 현재 상황에서 지금 당장 좋은 것만 고르는 방법
+
+# 예제 3-1. 거스름돈
 n = 1260
 count = 0
 
@@ -8,4 +12,7 @@ for coin in coin_types:
     count += n // coin
     n %= coin
 
-print(count)
+print(count) # 거스름 동전 개수
+
+# 공백을 기준으로 int형 변수 2개 입력받는 코드 -> map 을 이용하면 편리
+# n, k = map(int, input().split())
