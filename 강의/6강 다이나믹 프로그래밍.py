77e523335cfc7af39d85a496b7f1a35211ffaf0b
Minkyung Jang <70564788+skyups10@users.noreply.github.com>
2022-04-20 22:42:06 +0900

    Update 6강 다이나믹 프로그래밍.py

--- 강의/6강 다이나믹 프로그래밍.py
+++ 강의/6강 다이나믹 프로그래밍.py
@@ -11,3 +11,16 @@ def fibo(x):
   return fibo(x-1) + fibo(x-2)
 
 print(fibo(4))
+
+# 8-2.py 피보나치 함수 소스코드(재귀적)
+d = [0] * 100
+
+def fibo(x):
+  if x==1 or x==2:
+    return 1
+  if d[x] != 0:
+    return d[x]
+  d[x] = fibo(x-1) + fibo(x-2)
+  return d[x]
+
+print(fibo(99))
