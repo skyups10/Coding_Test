a04af9748b31a2e31203f8486bf34a9300725fa7
Minkyung Jang <70564788+skyups10@users.noreply.github.com>
2022-05-15 19:54:35 +0900

    Update 2. 그리디 & 구현.py

--- (이코테 2021) 이것이 취업을 위한 코딩 테스트다 with 파이썬/2. 그리디 & 구현.py
+++ (이코테 2021) 이것이 취업을 위한 코딩 테스트다 with 파이썬/2. 그리디 & 구현.py
@@ -81,5 +81,97 @@ prrint(result)
 # 머릿속에 있는 알고리즘을 소스코드로 바꾸는 과정
 
 # 예제 4-1. 상하좌우
+n = int(input())
+x, y = 1, 1
+plans = input().split()
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
+
+for plan in plans:
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+        if nx < 1 or ny < 1 or nx > n or ny > n:
+            continue
+        x, y = nx, ny
+        
+print(x, y)
+
 # 예제 4-2. 시각
-# 예제 4-3. 왕실의 나이트
+h = int(input())
+
+count = 0
+for i in range(h+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i)+str(j)+str(k):
+                count += 1
+print(count)
+
+# 예제. 왕실의 나이트
+input_data = input()
+row = int(input_data[1])
+column = int(ord(input_data[0])) - int(ord('a')) + 1
+
+steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+
+result = 9
+for step in steps:
+    next_row = row + step[0]
+    next_column = column + step[1]
+    
+    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
+        result += 1
+
+print(result)
+
+# 예제. 게임 개발
+n, m = map(int, input().split())
+
+d = [[0] * m for _ in range(n)]
+x, y, direction = map(int, input().split())
+d[x][y] = 1
+
+array = []
+for i in range(n):
+    array.append(list(map(int, input().split())))
+    
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def turn_left():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
+        
+count = 1
+turn_time = 0
+while True:
+    turn_left()
+    nx = x + dx[direciton]
+    ny = y + dy[direction]
+    
+    if d[nx][ny] == 0 and arrray[nx][ny] == 0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue
+    else:
+        turn_time += 1
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        if array[nx][ny] == 0:
+            x = nx
+            y = ny
+        else:
+            break
+        turn_time = 0
+        
+print(count)
