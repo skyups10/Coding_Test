c4dfeabc3ca964a3203d2f7834c71fabba2a2ac6
Minkyung Jang <70564788+skyups10@users.noreply.github.com>
2022-05-15 21:57:22 +0900

    Update 3. DFS & BFS.py

--- (이코테 2021) 이것이 취업을 위한 코딩 테스트다 with 파이썬/3. DFS & BFS.py
+++ (이코테 2021) 이것이 취업을 위한 코딩 테스트다 with 파이썬/3. DFS & BFS.py
@@ -133,4 +133,61 @@ visited = [False] * 9
 bfs(graph, 1, visited)
 
 # 예제. 음료수 얼려 먹기
+n, m = map(int, input().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+    
+def dfs(x,y):
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    if graph[x][y] == 0:
+        graph[x][y] == 1
+        dfs(x-1, y)
+        dfs(x, y-1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        return True
+    return False
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i,j) == True:
+            result += 1
+            
+print(result)
+
 # 예제. 미로 탈출
+from collections import deque
+
+n, m = map(int, input().split())
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+    
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x,y):
+    queue = deque()
+    queue.append(x,y)
+    
+    while queue:
+        x, y = queue.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+                
+    return graph[n-1][m-1]
+
+print(bfs(0,0))
