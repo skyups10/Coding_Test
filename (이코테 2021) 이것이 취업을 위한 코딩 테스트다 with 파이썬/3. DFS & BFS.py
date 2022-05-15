5256fafdaa993143e010c8a5564f34f39ec25b95
Minkyung Jang <70564788+skyups10@users.noreply.github.com>
2022-05-15 20:07:32 +0900

    Update 3. DFS & BFS.py

--- (이코테 2021) 이것이 취업을 위한 코딩 테스트다 with 파이썬/3. DFS & BFS.py
+++ (이코테 2021) 이것이 취업을 위한 코딩 테스트다 with 파이썬/3. DFS & BFS.py
@@ -1,12 +1,91 @@
 # 3강. DFS & BFS
+
+# 스택 5-1.py
+stack = []
+stack.append(5)
+stack.pop()
+
+print(stack) # 최하단 원소부터 출력
+print(stack[::-1]) # 최상단 원소부터 출력
+
+# 큐 5-2.py
+from collections import deque
+
+queue = deque()
+
+queue.append(5)
+queue.popleft()
+
+print(queue) # 먼저 들어온 순서대로 출력
+queue.reverse()
+print(queue) # 나중에 들어온 원소부터 출력
+
+# 재귀 함수 5-3.py
+def recursive_function():
+    print('재귀 함수를 호출합니다.')
+    recursive_function()
+  
+recursive_function()
+
+# 재귀 함수 종료 5-4.py
+def recursive_function():
+    if i == 100:
+      return
+    print(i, '번째 재귀 함수에서', i+1, '번째 재귀 함수를 호출합니다.')
+    recursive_function(i+1)
+    print(i, '번째 재귀 함수를 종료합니다.')
+  
+recursive_function(1)
+
+# 2가지 방식으로 구현한 팩토리얼 5-5.py
+# 반복적으로 구현한 n!
+def factorial_iterative(n):
+    result = 1
+    for i in range(1, n+1):
+      result *= i
+    return result
+  
+# 재귀적으로 구현한 n!
+def factorial_recursive(n):
+    if n <= 1:
+      return 1
+    return n * factorial_recursive(n-1)
+
+print('반복적으로 구현:', factorial_iteractive(5))
+print('재귀적으로 구현:', factorial_iteractive(5))
+
+# 인접 행렬 5-6.py -> 모든 관계를 저장하기 때문에 노드 개수가 많을수록 메모리 낭비 가능성 증가
+INF = 999999999
+
+graph = [
+  [0, 7, 5],
+  [7, 0, INF],
+  [5, INF, 0]
+]
+
+print(graph)
+
+# 인접 리스트 5-7.py -> 연결된 정보만을 저장하기 때문에 메모리 효율적으로 사용
+graph = [[] for _ in range(3)]
+
+# 노드 0에 연결된 노드 정보 저장(노드, 거리)
+graph[0].append((1, 7))
+graph[0].append((2, 5))
+
+graph[1].append((0, 7))
+
+graph[2].append((0, 5))
+
+print(graph)
+
 # DFS 5-8.py
 def dfs(graph, v, visited):
-  visitied[v] = True
-  print(v, end=' ')
+    visitied[v] = True
+    print(v, end=' ')
   
   for i in graph[v]:
     if not visitied[i]:
-      dfs(graph, i, visited)
+    dfs(graph, i, visited)
 
 graph = [
   [],
